caf7c287ad6b5208ede366bdb1a03c10e559858f
AshmalFatima <ashmalfatima25@gmail.com>
2025-10-02 12:07:27 +0500

    mobile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Landing from './pges/Landing';
-
+import { isMobile } from "react-device-detect";
+import Login from './pges/Login';
 function App() {
   return (
-   <Landing/>
+   <>
+    {isMobile ? <Login /> : <Landing />}
+   </>
   );
 }
 
